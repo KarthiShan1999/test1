dd3b33ce03e664f6027a199bc7e4bcbaef6e5172
DESKTOP-H7H5G4V\Karthi Sg <karthisg1999@gmail.com>
2023-02-09 23:44:22 +0530

    tast

--- UC1.sh
+++ UC1.sh
@@ -4,9 +4,10 @@
 # Generate a random number between 0 and 1
 attendance=$(( RANDOM % 2 ))
 
-# Check if the attendance is 0 or 1
+# Check if the       attendance is 0 or 1
 if [ $attendance -eq 0 ]; then
   echo "Employee is Absent"
 else
   echo "Employee is Present"
 fi
+# testat
